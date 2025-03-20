03609274bdeaed4e98fb1a47e0b4ae1461fa0f1f
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-03-20 21:44:20 +0600

    font name class added

---
+++ lib/utilities/app_font.dart
@@ -0,0 +1,9 @@
+/*
+* Created by Neloy on 20 March, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class AppFont{
+  static const String font500 = "font500";
+  static const String font600 = "font600";
+}
