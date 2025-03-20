09bebdd272e46cd3c7467221c2fff6c334e79f0b
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-03-20 21:44:28 +0600

    size class added

---
+++ lib/utilities/app_size.dart
@@ -0,0 +1,16 @@
+/*
+* Created by Neloy on 20 March, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter/material.dart';
+
+class AppSize{
+  // no gap:
+  static const SizedBox noGap = SizedBox();
+
+  // height:
+  static const SizedBox gapH05 = SizedBox(height: 05);
+  static const SizedBox gapH10 = SizedBox(height: 10);
+  static const SizedBox gapH15 = SizedBox(height: 15);
+}
