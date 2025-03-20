4866c16961bba4edd63b476a8bc242978c2dbaa1
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-03-20 21:44:02 +0600

    color class added

---
+++ lib/utilities/app_color.dart
@@ -0,0 +1,24 @@
+/*
+* Created by Neloy on 20 March, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color colorNotification = Color.fromRGBO(254, 71, 91, 1);
+  static const Color colorBottomAppBarActive = Color.fromRGBO(0, 161, 224, 1);
+  static const Color colorStatusNew = Color.fromRGBO(68, 169, 241, 1);
+  static const Color colorStatusNewBackground = Color.fromRGBO(227, 242, 253, 1);
+  static const Color colorStatusFirstResponseOverdue = Color.fromRGBO(255, 171, 0, 1);
+  static const Color colorStatusFirstResponseOverdueBackground = Color.fromRGBO(255, 171, 0, 0.12);
+  static const Color colorStatusCustomerResponded = Color.fromRGBO(90, 73, 180, 1);
+  static const Color colorStatusCustomerRespondedBackground = Color.fromRGBO(90, 73, 180, 0.12);
+  static const Color colorStatusOverdueBackground = Color.fromRGBO(255, 218, 222, 1);
+  static const Color colorTextTitle = Color.fromRGBO(59, 59, 59, 1);
+  static const Color colorTextNormal = Color.fromRGBO(121, 121, 121, 1);
+  static const Color colorTextTicketID = Color.fromRGBO(183, 183, 183, 1);
+  static const Color colorTextCheckbox = Color.fromRGBO(92, 93, 118, 1);
+  static const Color colorCardBackground = Color.fromRGBO(249, 250, 251, 1);
+  static const Color colorBorder = Color.fromRGBO(216, 224, 237, 1);
+}
