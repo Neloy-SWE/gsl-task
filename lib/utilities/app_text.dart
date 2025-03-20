c739d0a322097ec3caae082ac2433fed424f1196
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-03-20 21:44:36 +0600

    text class added

---
+++ lib/utilities/app_text.dart
@@ -0,0 +1,9 @@
+/*
+* Created by Neloy on 20 March, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class AppText{
+  static const String title = "Gain Solution Task";
+  static const String welcome = "Welcome";
+}
