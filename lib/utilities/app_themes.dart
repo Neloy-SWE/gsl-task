d8c62cf6f5ba75022f11f7904a3c436c975107de
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-03-20 21:44:46 +0600

    theme added

---
+++ lib/utilities/app_themes.dart
@@ -0,0 +1,55 @@
+/*
+* Created by Neloy on 20 March, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gsl_task/utilities/app_color.dart';
+import 'package:gsl_task/utilities/app_font.dart';
+
+class AppTheme {
+  static final get = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      color: Colors.white,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      titleTextStyle: TextStyle(
+        fontFamily: AppFont.font500,
+        color: AppColor.colorTextTitle,
+        fontSize: 22,
+      ),
+    ),
+    iconTheme: IconThemeData(
+      color: AppColor.colorTextTitle,
+    ),
+
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+        fontFamily: AppFont.font500,
+        fontSize: 22,
+        color: AppColor.colorTextTitle,
+      ),
+      titleMedium: TextStyle(
+        fontFamily: AppFont.font600,
+        fontSize: 16,
+        color: AppColor.colorTextTitle,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: AppFont.font500,
+        fontSize: 14,
+        color: AppColor.colorTextNormal,
+      ),
+
+      bodyMedium: TextStyle(
+        fontFamily: AppFont.font600,
+        fontSize: 12,
+        color: AppColor.colorTextNormal,
+      ),
+    ),
+  );
+}
