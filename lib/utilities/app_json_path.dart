6cdf51f94109e9ad4cdb3f8b8c3c21149ad90dcd
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-03-22 00:06:48 +0600

    contact json added

---
+++ lib/utilities/app_json_path.dart
@@ -0,0 +1,9 @@
+/*
+* Created by Neloy on 21 March, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class AppJsonPath{
+  static const String ticketJson = "assets/jsons/ticket.json";
+  static const String contactJson = "assets/jsons/contact.json";
+}
